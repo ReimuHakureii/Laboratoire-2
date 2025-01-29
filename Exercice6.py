5407b720cd19fef5abbc30bf81e9bf0200eb337c
ReimuHakureii <63125235+ReimuHakureii@users.noreply.github.com>
2025-01-29 12:05:40 -0500

    Exercice 6

---
+++ Exercice6.py
@@ -0,0 +1,5 @@
+nombre = input("Entrez un nombre : ")
+# Convertir l'entrée en entier pour effectuer la multiplication correctement
+nombre = int(nombre)
+resultat = nombre * 2
+print("Le double de votre nombre est :", resultat)
