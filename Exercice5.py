465019fe1353d5cdede47b51951b67f9f94d0ef6
ReimuHakureii <63125235+ReimuHakureii@users.noreply.github.com>
2025-01-29 13:26:12 -0500

    Docstring and rename

---
+++ Exercice5.py
@@ -0,0 +1,11 @@
+"""
+L'algorithme contrôle un système de chauffage en boucle.
+Description :
+- L'algorithme démarre et vérifie d'abord si l'exécution doit se terminer (condition "Fin?").
+    - Si la condition de fin est remplie, le processus s'arrête.
+    - Sinon, il poursuit.
+- Ensuite, l'algorithme vérifie si la température ambiante est inférieure à 20°C.
+    - Si la température est inférieure à 20°C, le chauffage est allumé.
+    - Sinon, le processus retourne au début de la boucle.
+- Ce cycle se répète jusqu'à ce que la condition de fin soit atteinte.
+"""
