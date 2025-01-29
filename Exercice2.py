dc0f57f38baebae6b43099871092b285c95feef9
ReimuHakureii <63125235+ReimuHakureii@users.noreply.github.com>
2025-01-29 11:59:14 -0500

    Exercice 2

---
+++ Exercice2.py
@@ -0,0 +1,6 @@
+nombre = input("Entrez un nombre : ")
+nombre_entier = int(nombre)
+nombre_flottant = float(nombre)
+
+print(f"Nombre entier : {nombre_entier}")
+print(f"Nombre flottant : {nombre_flottant}")
